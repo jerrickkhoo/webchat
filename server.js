2a44dcb60a2d7e3ab6f60483214f5e10ebe37f82
jerrickkhoo <jerrickkhoo@gmail.com>
2022-03-08 16:49:37 +0800

    scroll to bottom

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 require('dotenv').config()
 const app = express()
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 const MONGODB_URI = process.env.MONGODB_URI;
 const SECRET = process.env.SECRET
 
